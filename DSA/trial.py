8eb2645fe20ddcdbec5095848fe54f239ae75b3b
Gurmukh-Singh-4253 <gurmukh050304@gmail.com>
2024-12-14 12:21:18 +0530

    final commit before exams

--- DSA/trial.py
+++
@@ -1,9 +0,0 @@
-def f(n):
-    if n <= 1:
-        return 1 
-    if n % 2 == 0:
-        return (f(n//2))
-    return f(n//2) + f(n//2+1)
-
-for i in range(1,11):
-    print(f(i))
--- Python/Extra/imboredsoimmadomystuff.py
+++
@@ -1,9 +0,0 @@
-import random
-from matplotlib import pyplot as plt
-
-array = []
-for i in range(10000000):
-    array.append(random.random())
-
-plt.hist(array, bins=100)
-plt.show()
